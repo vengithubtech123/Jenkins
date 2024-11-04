e601ad2a2f1b84c8c68a3f78b72a9836ccbcaee2
VenumohanPotlabathin <venumohanpotlabathin@ext.icicibank.com>
2024-11-04 17:11:15 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+from openjdk:17
+
+EXPOSE 8080
+
+ADD target/SpringBootDeploy-1.0-SNAPSHOT.jar SpringDeploy-1.0.jar
+
+ENTRYPOINT["java", "-jar", "SpringDeploy-1.0.jar"]
+
---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ */
+
+@SpringBootApplication
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(App.class, args);
+        System.out.println( "Hello Jenkins!" );
+    }
+}
